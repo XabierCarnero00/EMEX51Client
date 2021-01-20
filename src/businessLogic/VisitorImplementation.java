cdf67b64fc551053caab27bd64c14e154b7a763a
2dam <markeluralde@gmail.com>
2021-01-20 09:49:10 +0100

    Controlador Visitor, Menu vista y genericController

---
+++ src/businessLogic/VisitorImplementation.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogic;
+import clientREST.VisitorREST;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.GenericType;
+import model.Visitor;
+
+/**
+ *
+ * @author Markel
+ */
+public class VisitorImplementation implements VisitorManager{
+    
+    /**
+     * REST users web client
+     */
+    private VisitorREST webClient;
+    /**
+     * Logger object used to control activity from class SectorManagerImplementation.
+     */
+    private static final Logger LOGGER=Logger.getLogger("SectorManagerImplentation");
+    /**
+     * Create a SectorManagerImplementation object. It constructs a web client for 
+     * accessing a RESTful service that provides business logic in an application
+     * server.
+     */
+    public VisitorImplementation(){
+        webClient=new VisitorREST();
+    }
+    /**
+     * This method returns a Collection of {@link Sector}.
+     * @return A List of {@link Sector}.
+     * @throws BusinessLogicException 
+     */
+    @Override
+    public List<Visitor> getAllVisitors() throws BusinessLogicException {
+        List<Visitor> visitors = null;
+        try{
+            LOGGER.info("Metodo getAllVisitors de la clase VisitorManagerImplementation.");
+            visitors = webClient.findAllVisitors(new GenericType<List<Visitor>>(){});
+        }catch(Exception e){
+            throw new BusinessLogicException(e.getMessage());
+        }
+        return visitors;       
+    }
+    /**
+     * This method returns a <code>Sector</code> with the same name as the one passed by parameter.
+     * @param name The name of a {@link Sector}.
+     * @return A sector object.
+     * @throws BusinessLogicException 
+     */
+    @Override
+    public List <Visitor> getVisitorsByName(String fullName) throws BusinessLogicException {
+        List <Visitor> sector = null;
+        try{
+            LOGGER.info("Metodo getVisitorsByName de la clase VisitorManagerImplementation.");
+            sector = webClient.findVisitorsByName(new GenericType<List<Visitor>>(){}, fullName);
+            if(sector.isEmpty())
+                throw new BusinessLogicException("No hay visitantes con el nombre "+fullName);
+        }catch(Exception e){
+            throw new BusinessLogicException(e.getMessage());
+        }
+        return sector;        
+    }
+    
+    /**
+     * This method updates data for an existing {@link Sector}.
+     * @param creature The {@link Sector} object to be updated.
+     * @throws BusinessLogicException 
+     */
+    @Override
+    public void updateVisitor(Visitor visitor) throws BusinessLogicException {
+        try{
+            LOGGER.info("Metodo updateVisitor de la clase VisitorManagerImplementation.");
+            webClient.edit(visitor);
+        }catch(Exception ex){
+            LOGGER.log(Level.SEVERE,"Exception updating visitor");
+            throw new BusinessLogicException("Error updating sector:\n"+ex.getMessage());
+        } 
+    }
+    /**
+     * This method deletes data for an existing {@link Sector}.
+     * @param creature The {@link Sector} object to be deleted.
+     * @throws BusinessLogicException 
+     */
+    @Override
+    public void deleteVisitor(Visitor visitor) throws BusinessLogicException {
+        try{
+            LOGGER.info("Metodo deleteVisitor de la clase VisitorManagerImplementation.");
+            webClient.remove(Integer.toString(visitor.getId()));
+        }catch(Exception ex){
+            LOGGER.log(Level.SEVERE,"Exception deleting visitor");
+            throw new BusinessLogicException("Error deleting visitor:\n"+ex.getMessage());
+        } 
+    }
+
+    
+}
---
+++ src/businessLogic/VisitorManager.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogic;
+
+import java.util.List;
+import model.Visitor;
+
+/**
+ *
+ * @author 2dam
+ */
+public interface VisitorManager {
+    /**
+     * This method returns a Collection of {@link Sector}.
+     * @return A List of {@link Sector}.
+     * @throws BusinessLogicException 
+     */
+    public List<Visitor> getAllVisitors() throws BusinessLogicException;
+    /**
+     * This method returns a <code>Visitor</code> with the same name as the one passed by parameter.
+     * @param name The name of a {@link Visitor}.
+     * @return A sector object.
+     * @throws BusinessLogicException 
+     */
+    public List <Visitor> getVisitorsByName(String name) throws BusinessLogicException;
+    /**
+     * This method updates data for an existing {@link Sector}.
+     * @param visitor The {@link Sector} object to be updated.
+     * @throws BusinessLogicException 
+     */
+    public void updateVisitor(Visitor visitor) throws BusinessLogicException;
+    /**
+     * This method deletes data for an existing {@link Sector}.
+     * @param visitor The {@link Sector} object to be deleted.
+     * @throws BusinessLogicException 
+     */
+    public void deleteVisitor(Visitor visitor) throws BusinessLogicException;
+    
+}
